282443bd9bb1def9044f34d7143e06291ba34fa4
Bryan_Cai <cai.bryan.au@gmail.com>
2023-05-30 13:32:13 +0930

    add-salerep-clasa-and-method

--- HelloWorld/HelloWorld/Developer.cs
+++ HelloWorld/HelloWorld/Developer.cs
@@ -3,4 +3,9 @@
 public class Developer: IEmployee
 {
     public string Name { get; set; }
+
+    public void canWriteCode()
+    {
+        Console.WriteLine("learn how to write clean code!");
+    }
 }
---
+++ HelloWorld/HelloWorld/SaleRep.cs
@@ -0,0 +1,6 @@
+﻿namespace HelloWorld;
+
+public class SaleRep: IEmployee
+{
+    public string Name { get; set; }
+}
